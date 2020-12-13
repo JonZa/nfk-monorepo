002ea85548092757dceff6a978001629df0cd515
Jon Muller <jon.muller@gmail.com>
2020-12-13 19:05:05 +1100

    assets

--- vue-nuxt/nuxt.config.js
+++ vue-nuxt/nuxt.config.js
@@ -1,3 +1,22 @@
+const path = require('path');
 export default {
 	target: 'static',
+	/*
+	 ** Nuxt.js dev-modules
+	 */
+	buildModules: ['@aceforth/nuxt-optimized-images'],
+	optimizedImages: {
+		optimizeImages: true
+	},
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config) {
+			config.resolve.alias['$shared'] = '../../shared';
+		}
+	}
 };
--- vue-nuxt/pages/index.vue
+++ vue-nuxt/pages/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Hello world!</h1>
+	<div class="what">
+		<h1>Hello world!</h1>
+		<img src="~$shared/cat.jpg?resize&size=480&format=webp" />
+	</div>
 </template>
+
+<style lang="scss">
+.what {
+	background: #00f;
+	background-image: url('~$shared/cat.jpg?resize&size=320&format=webp');
+}
+</style>
