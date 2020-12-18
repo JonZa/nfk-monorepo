d9712163f0c07320483ecac1f386985c0424b5f2
Jon Muller <jon.muller@gmail.com>
2020-12-18 14:10:47 +1100

    Form

--- vue-nuxt/pages/contact/index.vue
+++ vue-nuxt/pages/contact/index.vue
@@ -3,16 +3,14 @@
 		<h1>
 			Contact me
 		</h1>
-		<p>
-			You can email me at <a href="mailto:jon.muller@gmail.com">jon.muller@gmail.com</a> or give me a call me on <a href="tel:0466213376">0466&nbsp;213&nbsp;376</a>.
-		</p>
-		<!-- <form action="/" name="contact" method="POST" data-netlify="true" @submit.prevent="handleSubmit">
+		<p>You can email me at <a href="mailto:jon.muller@gmail.com">jon.muller@gmail.com</a> or give me a call me on <a href="tel:0466213376">0466&nbsp;213&nbsp;376</a>.</p>
+		<form name="contact" method="POST" data-netlify="true" @submit.prevent="handleSubmit">
 			<input type="hidden" name="form-name" value="contact" />
 			<label>Your Name: <input type="text" name="name"/></label>
 			<label>Your Email: <input type="email" name="email"/></label>
 			<label>Message: <textarea name="message"></textarea></label>
 			<button type="submit">Send</button>
-		</form> -->
+		</form>
 	</div>
 </template>
 
