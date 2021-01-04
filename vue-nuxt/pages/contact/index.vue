77a0837d7b066ee08bc90e9197ad5977467c6b26
Jon Muller <jon.muller@gmail.com>
2021-01-05 10:50:12 +1100

    Form stuff

--- vue-nuxt/pages/contact/index.vue
+++ vue-nuxt/pages/contact/index.vue
@@ -6,9 +6,9 @@
 		<p>You can email me at <a href="mailto:jon.muller@gmail.com">jon.muller@gmail.com</a> or give me a call me on <a href="tel:0466213376">0466&nbsp;213&nbsp;376</a>.</p>
 		<form name="contact" method="POST" @submit.prevent="handleSubmit">
 			<input type="hidden" name="form-name" value="contact" />
-			<label>Name: <input type="text" name="name"/></label>
-			<label>Email: <input type="email" name="email"/></label>
-			<label>Message: <textarea name="message" rows="3"></textarea></label>
+			<label>Name: <input type="text" v-model="formValues.name" name="name"/></label>
+			<label>Email: <input type="email" v-model="formValues.email" name="email"/></label>
+			<label>Message: <textarea name="message" v-model="formValues.message" rows="3"></textarea></label>
 			<button type="submit">Send</button>
 		</form>
 		<div id="potato"></div>
@@ -17,27 +17,31 @@
 
 <script>
 export default {
+	data() {
+		return {
+			formValues: {
+				name: 'Jon',
+				email: 'jon.muller@gmail.com',
+				message: ''
+			}
+		};
+	},
 	methods: {
 		encode(data) {
-			return Object.keys(data)
-				.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-				.join('&');
+			const formData = new FormData();
+			for (const key of Object.keys(data)) {
+				formData.append(key, data[key]);
+			}
+			return formData;
 		},
-		handleSubmit(e) {
+		handleSubmit() {
 			fetch('/', {
 				method: 'POST',
 				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-				body: this.encode({
-					...this.form
-				})
+				body: new URLSearchParams(this.encode(this.formValues)).toString()
 			})
-				.then(() => {
-					console.log('ok good');
-				})
-				.catch(() => {
-					console.log('oh no bad');
-				})
-				.then((document.getElementById('potato').innerHTML = 'OK great'));
+				.then(() => this.$router.push('thanks'))
+				.catch(error => console.log(error));
 		}
 	}
 };
---
+++ vue-nuxt/pages/thanks/index.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="about">
+		<h1>
+			Sweet
+		</h1>
+		<p>
+			Thanks for getting in touch, I'll get back to you shortly.
+		</p>
+	</div>
+</template>
